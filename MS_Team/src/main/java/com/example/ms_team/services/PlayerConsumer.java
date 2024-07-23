45b8ca7491e4a2e45809fecd7102bf7d86b41101
MedAmir98 <jemaamedamir@gmail.com>
2024-07-23 10:37:37 +0100

    add async kafka

---
+++ MS_Team/src/main/java/com/example/ms_team/services/PlayerConsumer.java
@@ -0,0 +1,16 @@
+package com.example.ms_team.services;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class PlayerConsumer {
+
+    @KafkaListener(topics = "product-topic", groupId = "my-group-id")
+    public void listen(String message) {
+        log.info("Received message: " + message);
+    }
+}
